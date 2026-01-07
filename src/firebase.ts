b44d3264788bc191a3b2921eaa241003dbd069ae
nobodyher <drdr84830@gmail.com>
2026-01-06 22:13:35 -0500

    aa

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyChTrRtbbblxGuD3ipMMLSN6y6blvaGCuM",
- authDomain: "blossom-gestion.firebaseapp.com",
- projectId: "blossom-gestion",
- storageBucket: "blossom-gestion.firebasestorage.app",
- messagingSenderId: "679370555994",
- appId: "1:679370555994:web:e901b4790834953a504e63"
+   apiKey: "AIzaSyChTrRtbbblxGuD3ipMMLSN6y6blvaGCuM",
+  authDomain: "blossom-gestion.firebaseapp.com",
+  projectId: "blossom-gestion",
+  storageBucket: "blossom-gestion.firebasestorage.app",
+  messagingSenderId: "679370555994",
+  appId: "1:679370555994:web:e901b4790834953a504e63"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
